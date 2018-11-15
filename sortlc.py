a6bf4e6464f132db0ab4836dd48c89a357277bbc
Lauren Magnuson <lauren@lpmagnuson.com>
2018-11-15 02:01:18 -0800

    cleanup

--- sortlc.py
+++ sortlc.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-import os
-import ftplib
-import sys
-#import callnumber
+
+#To run: name CSV file 'input.csv' and place in same directory as this file
+#The CSV file must have a header row and be encoded in UTF-8
+#In Terminal / Bash, run python sortlc.py or python3 sortlc.py
+#An output file will be created with a new column containing sortable call numbers
+#Instructions for installing pycallnumber: https://github.com/unt-libraries/pycallnumber
+
 import csv
-import re
-import operator
-import shutil
-import datetime
-from datetime import timedelta
-import zipfile
-import smtplib
 import pycallnumber as pycn
 
 with open('input.csv',encoding='utf-8', errors='ignore') as infile:  # Use 'with' to close files automatically
@@ -29,10 +25,6 @@ with open('input.csv',encoding='utf-8', errors='ignore') as infile:  # Use 'with
       row[0] = sortcall
       row.append(callnumber)
       all.append(row)
-      
-      #writer.writerow(all)
-      
-    
     sortedlist = sorted(all, key=operator.itemgetter(0))
     writer.writerows(sortedlist)
      
